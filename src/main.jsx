dfd11ddb08d6aaf4db414edcb117342d419292c8
Madhugit15 <madhushree1530@gmail.com>
2025-05-19 15:26:24 +0530

    some changes are made

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-import "suneditor/dist/css/suneditor.min.css";
-import SunEditor from "suneditor-react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-   
   </StrictMode>
 );
